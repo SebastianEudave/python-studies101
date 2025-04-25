0777cdc111b6f87b51dc29069b1d9dfaf6a74023
SebastianEudave <sebs.2901@gmail.com>
2025-04-25 07:44:42 -0600

    longest-substring-without-repeating-characters

--- leetcode/medium.py
+++ leetcode/medium.py
@@ -1,5 +1,34 @@
 class Solution:
     '''
+    Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+    '''
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        sLen = len(s)
+        maxCount = 0
+        leftPointer = 0
+        rightPointer = 1
+
+        if(sLen == 0 or sLen == 1): return sLen
+
+        currentSub = set()
+        currentSub.add(s[0])
+
+        '''
+        By using sliding window and a set we can track what values are part of the substring and what is our current substring 
+        '''
+        while(rightPointer != len(s)):
+            newChar = s[rightPointer]
+            while newChar in currentSub:
+                currentSub.remove(s[leftPointer])
+                leftPointer += 1
+            currentSub.add(newChar)
+            currentSubSize = rightPointer - leftPointer + 1
+            if currentSubSize > maxCount: maxCount = currentSubSize
+            rightPointer += 1
+
+        return maxCount
+
+    '''
     Problem: https://leetcode.com/problems/group-anagrams/
     '''
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
